49b118e60c33d8cbabed19d3cb53a1681784e3da
luis <151880089+shuuchakai@users.noreply.github.com>
2024-04-12 12:07:12 -0600

    Update createTransport.util.js

--- src/utils/createTransport.util.js
+++ src/utils/createTransport.util.js
@@ -2,18 +2,19 @@ import nodemailer from 'nodemailer';
 
 const createTransport = () => {
     return nodemailer.createTransport({
-        service: 'hotmail',
-        secure: false,
         host: 'smtp.office365.com',
-        tls: {
-            ciphers: "SSLv3",
-        },
         port: 587,
+        secure: false, // `false` for port 587, `true` for 465, Office 365 uses STARTTLS on 587
         auth: {
             user: process.env.EMAIL_USERNAME,
             pass: process.env.EMAIL_PASSWORD,
         },
+        tls: {
+            // No specific ciphers necessary
+            rejectUnauthorized: true
+        }
     });
 };
+
 
 export default createTransport;
